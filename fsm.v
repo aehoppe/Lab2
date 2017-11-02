5d85452cf3cca2a686ddba2c441d53cad9e7744d
juicyslew <william.derksen@students.olin.edu>
2017-11-01 21:10:14 -0400

    FSM first pass

--- fsm.v
+++ fsm.v
@@ -17,39 +17,50 @@ module FSM
   reg[2:0] counter;
   //reg[7:0] bitmap;
 
-  reg [5:0] state;
-	localparam IDLE = 6'b000001, ADDRESS = 6'b000010, ADDRESS_WRITE = 6'b000100, WRITE = 6'b001000, READ_START = 6'b010000,  READ = 6'b100000, DONE;
+  reg [7:0] state;
+
+	localparam
+  IDLE = 8'b00000001,
+  ADDRESS = 8'b00000010,
+  ADDRESS_WRITE = 8'b00000100,
+  WRITE_WAIT = 8'b00001000,
+  WRITE_MEM = 8'b00010000,
+  READ_START = 8'b00100000,
+  READ = 8'b01000000,
+  DONE = 8'b10000000;
 
   always @(posedge sclk) begin
-    if(state == 6'bx) begin
+    if(state == 8'bx) begin
       state <= IDLE;
     end else begin
       case(state)
-        IDLE:
+        IDLE: begin
           if(cs_pin == 0) begin
             state <= ADDRESS;
             counter <= 3'b000;
           end else if (cs_pin == 1) begin
             state <= IDLE;
           end
+        end
 
-        ADDRESS:
+        ADDRESS: begin
           if(counter < 5) begin
             state <= ADDRESS;
             counter <= counter + 3'b001;
           end else begin
             state <= ADDRESS_WRITE;
           end
+        end
 
-        ADDRESS_WRITE:begin
+        ADDRESS_WRITE: begin
           // addr_WE <= 1;
-
           counter <= 3'b000;
           if(shiftReg0) begin
             state <= READ;
           end else begin
-            state <= WRITE;
+            state <= WRITE_WAIT;
           end
+        end
 
         READ_START: begin
           //SR_WE
@@ -57,7 +68,7 @@ module FSM
           state <= READ;
         end
 
-        READ:
+        READ: begin
           //miso_BUFE
           if(counter < 6) begin
             state <= READ;
@@ -65,21 +76,92 @@ module FSM
           end else begin
             state <= DONE;
           end
+        end
 
-        WRITE_START:
-          if(counter)
+        WRITE_WAIT: begin
+          if(counter < 7) begin
+            state <= WRITE_WAIT;
+          end else begin
+            state <= WRITE_MEM;
+          end
+        end
 
-        DONE:
+        WRITE_MEM: begin
+          state <= DONE;
+          //DM_WE
+        end
+
+        DONE: begin
           if(cs_pin == 0)begin
             state <= IDLE;
           end else begin
             state <= DONE;
           end
+        end
 
-        endcase
+      endcase
+      case (state)
+        //driving - follow traffic laws
+        IDLE: begin
+          addr_WE <= 0;
+          miso_BUFE <= 0;
+          DM_WE <= 0;
+          SR_WE <= 0;
+        end
 
 
-      end
+        ADDRESS: begin
+          addr_WE <= 0;
+          miso_BUFE <= 0;
+          DM_WE <= 0;
+          SR_WE <= 0;
+        end
+
+
+        ADDRESS_WRITE: begin
+          addr_WE <= 1;
+          miso_BUFE <= 0;
+          DM_WE <= 0;
+          SR_WE <= 0;
+        end
+
+        READ_START:begin
+          addr_WE <= 0;
+          miso_BUFE <= 1;
+          DM_WE <= 0;
+          SR_WE <= 1;
+        end
+
+        READ:begin
+          addr_WE <= 0;
+          miso_BUFE <= 1;
+          DM_WE <= 0;
+          SR_WE <= 0;
+        end
+
+        WRITE_WAIT:begin
+          addr_WE <= 0;
+          miso_BUFE <= 0;
+          DM_WE <= 0;
+          SR_WE <= 0;
+        end
+
+        WRITE_MEM:begin
+          addr_WE <= 0;
+          miso_BUFE <= 0;
+          DM_WE <= 1;
+          SR_WE <= 0;
+        end
+
+        DONE:begin
+          addr_WE <= 0;
+          miso_BUFE <= 0;
+          DM_WE <= 0;
+          SR_WE <= 0;
+        end
+
+      endcase
     end
+  end
 
 endmodule
