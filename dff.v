6273668f38eba54d6977128770b1bab9644b4934
Alexander Hoppe <alexanderehoppe@gmail.com>
2017-11-02 22:13:06 -0400

    implemented SPI memory tests

--- dff.v
+++ dff.v
@@ -8,13 +8,12 @@ module dff
 (
 input                   clk,            // Global FPGA Clock
 input                   clockEdge,      // Device Clock Edge
-input                   writeEnable,    // 1 = pass D through to Q
 input       [width-1:0] D,              // Input
 output reg  [width-1:0] Q               // Output
     );
 
     always @(posedge clk) begin
-        if (writeEnable && clockEdge) begin
+        if (clockEdge) begin
             Q <= D;
         end
     end
